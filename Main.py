628824ed00d0a0bccb1eebdb23d88da578614261
Paulo Rogério <paulorogmelo@gmail.com>
2023-01-22 18:26:11 -0300

    shttps://github.com/RealMeloLima/GenshinBot

--- Main.py
+++ Main.py
@@ -242,9 +242,5 @@ async def build(context):
     await context.message.channel.send(file=dc.File("collei.png"))
 
 
-<<<<<<< HEAD
+client.run('MTA2NjQxNTkyMzQyODM0MzkxOQ.GIqaYr.Kjz7cklmtf4iHgUA7jL_9z_0azUMRW6mT69_bs')
 
-client.run('MTA2NjQxNTkyMzQyODM0MzkxOQ.GgexUJ.XAG1JrPHxAsVfddKPBmDYKklD_Uo1PWBK6YCH0')
-=======
-client.run('YOUR TOKEN GOES HERE)
->>>>>>> e0bbd13442a2580a919f586c66ba6a0c6681b747
