c454aeb607b0c24ab41fd08b1486f5d207238026
Paulo Rogério <80416920+RealMeloLima@users.noreply.github.com>
2023-02-07 10:40:25 -0300

    New Build List
    Now the Build List can only have 10 characters per page

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import asyncio
 
 # INTENTS
 intents = dc.Intents.all()
+
 client = commands.Bot(command_prefix='?', intents = intents)
 
 # TESTE
@@ -14,17 +15,13 @@ async def on_ready():
     print(f'Im ready and logged on {client.user}')
 
 # LISTA DOS PERSONAGENS
-items = ['Albedo', 'Alhaitham', 'Ayaka' , 'Bennett', 'Childe' , 'Collei' ,'Cyno', 'Diluc', 'Eula' , 'Faruzan', 'Fischl', 'Ganyu', 'Gorou' ,'Heizou', 'Hu tao', 'Itto', 'Kazuha', 'Mona', 'Qiqi', 'Rainden', 'Sara', 'Sayu', 'Shinobu', 'Sucrose', 'Thoma', 'Tighnari', 'Venti', 'Wanderer', 'Xiao', 'Xinyan', 'Yanfei', 'YaoYao', 'Yelan', 'Yoimiya', 'Zhongli']
-
-paginator = Paginator()
-paginator.items_per_page = 10
-for item in items:
-    paginator.add_line(item)
+items = ['Albedo', 'Alhaitham', 'Ayaka' , 'Bennett', 'Childe' , 'Collei' ,'Cyno', 'Diluc', 'Eula' , 'Faruzan', 'Fischl', 'Ganyu', 'Gorou' ,'Heizou', 'Hu tao', 'Itto', 'Kazuha', 'Mona', 'Qiqi', 'Rainden', 'Sara', 'Sayu', 'Shinobu', 'Sucrose'  , 'Thoma', 'Tighnari', 'Venti', 'Wanderer', 'Xiao', 'Xinyan', 'Yanfei', 'YaoYao', 'Yelan', 'Yoimiya', 'Zhongli']
+pages = [items[i:i + 10] for i in range(0, len(items), 10)]
 
 @client.command(name='Builds')
 async def list_items(ctx):
     current_page = 0
-    message = await ctx.send(paginator.pages[current_page])
+    message = await ctx.send("\n" + "\n".join(pages[current_page]) + "")
     await message.add_reaction("⬅️")
     await message.add_reaction("➡️")
 
@@ -36,13 +33,12 @@ async def list_items(ctx):
             reaction, user = await client.wait_for('reaction_add', timeout=30.0, check=check)
             if str(reaction.emoji) == "⬅️":
                 current_page = max(current_page - 1, 0)
-                await message.edit(content=paginator.pages[current_page])
+                await message.edit(content="\n" + "\n".join(pages[current_page]) + "")
             elif str(reaction.emoji) == "➡️":
-                current_page = min(current_page + 1, len(paginator.pages) - 1)
-                await message.edit(content=paginator.pages[current_page])
+                current_page = min(current_page + 1, len(pages) - 1)
+                await message.edit(content="\n" + "\n".join(pages[current_page]) + "")
         except asyncio.TimeoutError:
             break
-
 # IMAGEM DO PERSONAGEM   
 for item in items:
     def build_func(item):
@@ -55,6 +51,5 @@ for item in items:
             await ctx.message.channel.send(f'Here you have your {item} build, enjoy it :)')
             await ctx.message.channel.send(file=dc.File(file_name))
     build_func(item)
-
 
 client.run('YOUR TOKEN GOES HERE')
